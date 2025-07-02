22f7a2778c71aa4c35190da93b9dd2785e2b65b9
ndryqzn <canilaoquizonandrey@gmail.com>
2025-07-02 14:29:28 +0800

    Updated the calculate() method

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // Output
 const output = document.getElementById('output');
-// Silver buttons
-const clear = document.getElementById('clear');
-const posOrneg = document.getElementById('posOrneg');
-const percent = document.getElementById('percent');
-// Number buttons
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
-const point = document.getElementById('point');
-// Operator buttons
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const equal = document.getElementById('equal');
+
+function display(val){
+    output.innerHTML += val;
+}
+
+function clearDisplay(){
+    output.innerHTML = '';
+}
+
+function calculate(){
+    let expression = output.innerHTML;
+    expression = expression.replaceAll("÷", "/").replaceAll("×", "*");
+    if(isNaN(expression[expression.length - 1])){
+        console.log(`The trailing operator "${expression[expression.length - 1]}" has been removed.`);
+        expression = expression.slice(expression.length - expression.length, expression.length - 1);
+        // console.log(eval(expression));
+        clearDisplay();
+        display(eval(expression));
+    }else{
+        clearDisplay();
+        display(eval(expression));
+    }
+    
+}
